5b9bd99f871a42e688503d05d8aeff8e43bd20cd
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-04 22:15:36 +0600

    feat(getOne): get one service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
 
@@ -16,46 +16,61 @@ const uri = `mongodb+srv://${userName}:${userPassword}@cluster0.40hja.mongodb.ne
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const serviceDB = client.db("serviceDB");
-    const serviceCollection = serviceDB.collection("serviceCollection");
+    try {
+        await client.connect();
+        const serviceDB = client.db("serviceDB");
+        const serviceCollection = serviceDB.collection("serviceCollection");
 
-    app.post("/services", async (req, res) => {
-        const servicesData = req.body;
-        const result = await serviceCollection.insertOne(servicesData)
-        console.log(result)
-        res.send(result)
-      })
+        app.post("/services", async (req, res) => {
+            const servicesData = req.body;
+            const result = await serviceCollection.insertOne(servicesData)
+            console.log(result)
+            res.send(result)
+        })
 
 
-      app.get("/services", async (req, res) => {
-        const servicesData = serviceCollection.find();
-        const result = await servicesData.toArray()
-        console.log(result)
-        res.send(result)
-      })
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-  }
+        app.get("/services", async (req, res) => {
+            const servicesData = serviceCollection.find();
+            const result = await servicesData.toArray()
+            console.log(result)
+            res.send(result)
+        })
+
+        app.get("/shoes/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await serviceCollection.findOne({ _id: new ObjectId(id) })
+            console.log(result)
+            res.send(result)
+          })
+
+        // app.patch("/services/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     const updateService = req.body;
+        //     const result = await serviceCollection.updateOne({ _id: new ObjectId(id) }, { $set: updateService })
+        //     console.log(result)
+        //     res.send(result)
+        // })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 
