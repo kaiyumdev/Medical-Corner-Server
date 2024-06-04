c0038fc3a021c0525caeb9e8657154d38f48e280
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-04 19:24:38 +0600

    feat(post): post method working successfully to create new service

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 require('dotenv').config();
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 
 const port = process.env.PORT || 3000;
+const userName = process.env.DB_USERNAME
+const userPassword = process.env.DB_PASSWORD
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${userName}:${userPassword}@cluster0.40hja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceDB = client.db("serviceDB");
+    const serviceCollection = serviceDB.collection("serviceCollection");
+
+    app.post("/services", async (req, res) => {
+        const servicesData = req.body;
+        const result = await serviceCollection.insertOne(servicesData)
+        console.log(result)
+        res.send(result)
+      })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
