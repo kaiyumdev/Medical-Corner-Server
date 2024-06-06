7638ae97a4821a38e6605dcbcbe8c8e817254b41
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-06 11:30:09 +0600

    feat(updated): solved the issue updated the code

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-require('dotenv').config();
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const jwt = require("jsonwebtoken");
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
-const userName = process.env.DB_USERNAME
-const userPassword = process.env.DB_PASSWORD
+const userName = process.env.DB_USERNAME;
+const userPassword = process.env.DB_PASSWORD;
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json());
-
+app.use(cors());
+app.use(express.json());
 
 function createToken(user) {
     const token = jwt.sign(
-      {
-        email: user.email,
-      },
-      "secret",
-      { expiresIn: "7d" }
+        { email: user.email },
+        "secret",
+        { expiresIn: "7d" }
     );
     return token;
-  }
+}
 
-  function verifyToken(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    const verify = jwt.verify(token, "secret");
-    if (!verify?.email) {
-      return res.send("You are not authorized");
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send("Authorization header missing");
     }
-    req.user = verify.email;
-    next();
-  }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).send("Token missing in authorization header");
+    }
+
+    try {
+        const verify = jwt.verify(token, "secret");
+        if (!verify?.email) {
+            return res.status(403).send("You are not authorized");
+        }
+        req.user = verify.email;
+        next();
+    } catch (error) {
+        return res.status(401).send("Invalid token");
+    }
+}
 
 const uri = `mongodb+srv://${userName}:${userPassword}@cluster0.40hja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -55,58 +62,56 @@ async function run() {
         const serviceCollection = serviceDB.collection("serviceCollection");
         const userCollection = userDB.collection("userCollection");
 
-        //services
+        // Services
         app.post("/services", verifyToken, async (req, res) => {
             const servicesData = req.body;
-            const result = await serviceCollection.insertOne(servicesData)
-            res.send(result)
-        })
-
+            const result = await serviceCollection.insertOne(servicesData);
+            res.send(result);
+        });
 
         app.get("/services", async (req, res) => {
             const servicesData = serviceCollection.find();
-            const result = await servicesData.toArray()
-            res.send(result)
-        })
+            const result = await servicesData.toArray();
+            res.send(result);
+        });
 
         app.get("/services/:id", async (req, res) => {
             const id = req.params.id;
-            const result = await serviceCollection.findOne({ _id: new ObjectId(id) })
-            res.send(result)
-        })
+            const result = await serviceCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
 
         app.patch("/services/:id", verifyToken, async (req, res) => {
             const id = req.params.id;
             const updateService = req.body;
-            const result = await serviceCollection.updateOne({ _id: new ObjectId(id) }, { $set: updateService })
-            res.send(result)
-        })
+            const result = await serviceCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updateService }
+            );
+            res.send(result);
+        });
 
         app.delete("/services/:id", verifyToken, async (req, res) => {
             const id = req.params.id;
-            console.log(id)
-            const result = await serviceCollection.deleteOne({ _id: new ObjectId(id) })
-            console.log(result)
-            res.send(result)
-        })
+            const result = await serviceCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
 
-
-        //   users
-        app.post("/user",verifyToken, async (req, res) => {
+        // Users
+        app.post("/user", async (req, res) => {
             const userData = req.body;
-            const token = createToken(userData)
-            console.log(token)
-            const isUserExist = await userCollection.findOne({ email: userData?.email })
+            const token = createToken(userData);
+            const isUserExist = await userCollection.findOne({ email: userData?.email });
             if (isUserExist?._id) {
                 return res.send({
-                  statu: "success",
-                  message: "Login success",
-                  token,
+                    status: "success",
+                    message: "Login success",
+                    token,
                 });
-              }
-              await userCollection.insertOne(user);
-              return res.send({ token });
-        })
+            }
+            await userCollection.insertOne(userData);
+            return res.send({ token });
+        });
 
         app.get("/user/get/:id", async (req, res) => {
             const id = req.params.id;
@@ -116,7 +121,6 @@ async function run() {
 
         app.get("/user/:email", async (req, res) => {
             const email = req.params.email;
-            console.log(email)
             const result = await userCollection.findOne({ email });
             res.send(result);
         });
@@ -129,24 +133,20 @@ async function run() {
                 { $set: userData },
                 { upsert: true }
             );
-            console.log(result)
             res.send(result);
         });
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
+        // Ensure proper cleanup
     }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('This is Medical-Corner Server by Mine Seriously!')
-})
+    res.send('This is Medical-Corner Server by Mine Seriously!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-
-
+    console.log(`Example app listening on port ${port}`);
+});
